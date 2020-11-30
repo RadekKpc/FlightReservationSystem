ff80c5983324d59c1fc16ed6702336a00529d625
RadekKpc <radek4ec@gmail.com>
2020-11-30 18:55:25 +0100

    Add Client endpoint

--- backend/src/main/java/com/wesolemarcheweczki/backend/model/Client.java
+++ backend/src/main/java/com/wesolemarcheweczki/backend/model/Client.java
@@ -4,19 +4,29 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Client {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
+
+    @NotNull
     private String firstName;
+
+    @NotNull
     private String lastName;
 
+    @NotNull
+    @Email(message = "Not appropriate email")
+    private String email;
 
-    public Client(String firstName, String lastName) {
+    public Client(String firstName, String lastName,String email) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.email = email;
     }
     public Client() {
     }
@@ -34,5 +44,13 @@ public class Client {
 
     public String getLastName() {
         return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
