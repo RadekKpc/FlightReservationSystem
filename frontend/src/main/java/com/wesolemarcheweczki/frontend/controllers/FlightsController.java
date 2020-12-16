fe9c96430f0bae9f18d0bff707e92274c90aba44
WikKam <kamykikamienie@gmail.com>
2020-12-16 16:15:21 +0100

    Dodanie metodyCreateGetTask i rozpoczęcie pracy nad edycją tabelki

--- frontend/src/main/java/com/wesolemarcheweczki/frontend/controllers/FlightsController.java
+++ frontend/src/main/java/com/wesolemarcheweczki/frontend/controllers/FlightsController.java
@@ -3,39 +3,68 @@ package com.wesolemarcheweczki.frontend.controllers;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.wesolemarcheweczki.frontend.model.Carrier;
 import com.wesolemarcheweczki.frontend.model.Flight;
+import com.wesolemarcheweczki.frontend.model.Location;
 import com.wesolemarcheweczki.frontend.restclient.RestClient;
+import javafx.beans.property.SimpleSetProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.ComboBoxTableCell;
+import javafx.scene.control.cell.TextFieldTableCell;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Locale;
 import java.util.ResourceBundle;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
 
 public class FlightsController implements Initializable {
 
+    private ObservableList<Flight> listOfFlights;
+    private ObservableList<Carrier> listOfCarriers = FXCollections.observableArrayList();
+    private ObservableList<Location> listOfLocations = FXCollections.observableArrayList();
+
+    public TableColumn freeColumn;
+    public TableColumn bookedColumn;
+    public TableColumn<Flight, String> priceColumn;
+    public TableColumn<Flight, String> arrivalColumn;
+    public TableColumn<Flight, String> departureColumn;
+    public TableColumn<Flight, String> fromColumn;
+    public TableColumn <Flight, String> toColumn;
+    public TableColumn<Flight, String> carrierColumn;
+    public TableView<Flight> dataTable;
     @FXML
     private VBox flightContainer;
 
+    @FXML
+    
+    
     private final RestClient restClient = new RestClient();
 
-    public void loadFlights() throws IOException, InterruptedException {
-        flightContainer.getChildren().clear();
-        String response = restClient.getObject("/flight");
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.registerModule(new JavaTimeModule());
-        List<Flight> valuesList = mapper.readValue(response, new TypeReference<>(){});
-        System.out.println(valuesList);
-        for(Flight v : valuesList) {
-            System.out.println(v);
-            addFlightToList(v);
-        }
-    }
+    private final ExecutorService executorService = Executors.newCachedThreadPool();
+
+    private final Task<List<Flight>> getFlights = restClient.createGetTask("/flight",Flight.class);
+
+    private final Task<List<Carrier>> getCarriers = restClient.createGetTask("/carrier", Carrier.class);
+
+    private final Task<List<Location>> getLocation = restClient.createGetTask("/location", Location.class);
 
     public void addFlightToList(Flight f) throws IOException { // add flight to VBox list
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/views/Item.fxml"));
@@ -45,13 +74,77 @@ public class FlightsController implements Initializable {
         flightContainer.getChildren().add(root);
     }
 
+    private void initColumns(){
+        dataTable.setEditable(true);
+        priceColumn.setCellValueFactory(cellData -> new SimpleStringProperty(Integer.toString(cellData.getValue().getBaseCost())));
+        priceColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        arrivalColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getArrival().toString()));
+        arrivalColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        departureColumn.setCellValueFactory(cellData ->  new SimpleStringProperty(cellData.getValue().getDeparture().toString()));
+        departureColumn.setCellFactory(TextFieldTableCell.forTableColumn());
+        fromColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getSource().getCity()));
+        toColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getDestination().getCity()));
+        carrierColumn.setCellValueFactory(cellData -> new SimpleStringProperty(cellData.getValue().getCarrier().getName()));
+        priceColumn.setOnEditCommit(
+                (TableColumn.CellEditEvent<Flight, String> t) ->
+                        ( t.getTableView().getItems().get(
+                                t.getTablePosition().getRow())
+                        ).setBaseCost(Integer.parseInt(t.getNewValue()))
+        );
+    }
+
     @Override
     @FXML
     public void initialize(URL location, ResourceBundle resources) {
-        try {
-            this.loadFlights();
-        } catch (IOException | InterruptedException e) {
-            e.printStackTrace();
-        }
+        //TODO: zmienić z ComboBoxTableCell.forTableColumn na coś customowego co radzi sobie z obiektami
+        initColumns();
+        getFlights.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+            @Override
+            public void handle(WorkerStateEvent event) {
+                System.out.println("succeeded");
+                var flightList = getFlights.getValue();
+                listOfFlights = FXCollections.observableArrayList(flightList);
+                dataTable.setItems(listOfFlights);
+            }
+        });
+        getCarriers.setOnSucceeded(event -> {
+            var carrierList = getCarriers.getValue();
+            System.out.println(carrierList);
+            listOfCarriers.addAll(carrierList);
+            carrierColumn.setCellFactory(ComboBoxTableCell.forTableColumn(
+                    FXCollections
+                            .observableArrayList(
+                                    listOfCarriers
+                                            .stream()
+                                            .map(Carrier::getName)
+                                            .collect(Collectors.toList()))));
+        });
+
+        getLocation.setOnSucceeded(event -> {
+            System.out.println("location success");
+            var locationsList = getLocation.getValue();
+            System.out.println("locations " + locationsList);
+            listOfLocations.addAll(locationsList);
+            fromColumn.setCellFactory(ComboBoxTableCell.forTableColumn(
+                    FXCollections
+                            .observableArrayList(
+                                    listOfLocations
+                                            .stream()
+                                            .map(Location::getAirportId)
+                                            .collect(Collectors.toList()))));
+
+            toColumn.setCellFactory(ComboBoxTableCell.forTableColumn(
+                    FXCollections
+                            .observableArrayList(
+                                    listOfLocations
+                                            .stream()
+                                            .map(Location::getAirportId)
+                                            .collect(Collectors.toList()))));
+
+        });
+        executorService.submit(getCarriers);
+        executorService.submit(getFlights);
+        executorService.submit(getLocation);
     }
+
 }
--- frontend/src/main/java/com/wesolemarcheweczki/frontend/restclient/RestClient.java
+++ frontend/src/main/java/com/wesolemarcheweczki/frontend/restclient/RestClient.java
@@ -2,18 +2,25 @@ package com.wesolemarcheweczki.frontend.restclient;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.wesolemarcheweczki.frontend.model.Flight;
+import javafx.concurrent.Task;
 import org.apache.commons.codec.binary.Base64;
+import org.yaml.snakeyaml.events.CollectionStartEvent;
+
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.nio.charset.StandardCharsets;
+import java.util.Collections;
 import java.util.List;
 
 
-public class RestClient {
+public class RestClient<T> {
     private static HttpClient httpClient = HttpClient.newHttpClient();
     private String url = "http://localhost:8080/api";
     private ObjectMapper mapper = new ObjectMapper();
@@ -56,6 +63,24 @@ public class RestClient {
         } else {
             return null;
         }
+    }
+
+    public Task<List<T>> createGetTask(String endpoint, Class<T> className){
+        return new Task<List<T>>() {
+
+            @Override
+            protected List<T> call() throws Exception {
+                String response = getObject(endpoint);
+                System.out.println(response);
+                ObjectMapper mapper = new ObjectMapper();
+                mapper.registerModule(new JavaTimeModule());
+                TypeFactory typeFactory = mapper.getTypeFactory();
+                CollectionType type = typeFactory.constructCollectionType(List.class, className);
+                List<T> valueList = mapper.readValue(response, type);
+                System.out.println(valueList);
+                return valueList;
+            }
+        };
     }
 
 }
