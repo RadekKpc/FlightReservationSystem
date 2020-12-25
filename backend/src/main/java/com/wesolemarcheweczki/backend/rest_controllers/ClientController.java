fa43bdf5aa30f8e2dee1cfc5c8ae8b8f4c277197
RadekKpc <radek4ec@gmail.com>
2020-12-25 21:27:01 +0100

    add administrative endponit to add admins

--- backend/src/main/java/com/wesolemarcheweczki/backend/rest_controllers/ClientController.java
+++ backend/src/main/java/com/wesolemarcheweczki/backend/rest_controllers/ClientController.java
@@ -2,8 +2,15 @@ package com.wesolemarcheweczki.backend.rest_controllers;
 
 import com.wesolemarcheweczki.backend.model.Client;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+import static com.wesolemarcheweczki.backend.rest_controllers.helpers.Responses.*;
 
 @RestController
 @RequestMapping("/api/client")
@@ -14,5 +21,31 @@ public class ClientController extends AbstractRestController<Client> {
         super();
     }
 
+    @Override
+    @PostMapping(consumes = "application/json") //This one always creates new instance
+    public ResponseEntity<Void> create(@Valid @RequestBody Client received) {
+        try {
+            received.setRole("ROLE_USER");
+
+            if (DAO.add(received.copy())) {
+                return ok();
+            }
+            return forbidden();
+        } catch (Exception e) {
+            return badRequest();
+        }
+    }
+
+    @PostMapping(path = "/admin", consumes = "application/json") //This one always creates new instance
+    public ResponseEntity<Void> createWithRole(@Valid @RequestBody Client received) {
+        try {
+            if (DAO.add(received.copy())) {
+                return ok();
+            }
+            return forbidden();
+        } catch (Exception e) {
+            return badRequest();
+        }
+    }
 
 }
