2948f0e176b9dbdab176a848f302ac92f4362568
RadekKpc <radek4ec@gmail.com>
2021-01-06 01:04:26 +0100

    Add collision endpoint

--- backend/src/main/java/com/wesolemarcheweczki/backend/repository/FlightRepository.java
+++ backend/src/main/java/com/wesolemarcheweczki/backend/repository/FlightRepository.java
@@ -1,9 +1,19 @@
 package com.wesolemarcheweczki.backend.repository;
 
+import com.wesolemarcheweczki.backend.model.Client;
 import com.wesolemarcheweczki.backend.model.Flight;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface FlightRepository extends JpaRepository<Flight,Integer> {
+    @Query("SELECT COUNT(*) FROM Order AS o " +
+            "JOIN Ticket t on t.order = o " +
+            "JOIN Flight f on t.flight = f " +
+            "WHERE o.client = ?3 AND NOT " +
+            " ( ?1 <= f.arrival OR f.departure > ?2)")
+    int getCollisionWithFlightForGivenClient(LocalDateTime departure, LocalDateTime arrival, Client client);
 }
